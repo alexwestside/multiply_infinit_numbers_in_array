fa6d66e4302618bde434cb64cb6a7bd6f9486936
orizhiy <sklif.85@gmail.com>
2017-04-22 18:01:24 +0300

    int_array

---
+++ cmake-build-debug/CMakeFiles/GL_task_multiply_numbers_int_array_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GL_task_multiply_numbers_int_array_.dir/main.cpp.o"
+  "GL_task_multiply_numbers_int_array_.pdb"
+  "GL_task_multiply_numbers_int_array_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GL_task_multiply_numbers_int_array_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GL_task_multiply_numbers_int_array_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GL_task_multiply_numbers_int_array_.
+# This may be replaced when dependencies are built.
