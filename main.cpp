aa9236b20489a8ed7816679c1a24bcaa51d0b5d1
orizhiy <sklif.85@gmail.com>
2017-04-24 12:54:08 +0300

    end_2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 void ft_error(int id)
 {
@@ -23,6 +26,16 @@ void ft_error(int id)
  * Function convert character to integer
  */
 
+
+void print_answer(int *arr, size_t len)
+{
+	ofstream result;
+	result.open("result.txt");
+	while (len--)
+		result << arr[len];
+	result.close();
+}
+
 int atoi_char(char c)
 {
 	int num = 0;
@@ -39,13 +52,14 @@ void mult_infinit(int *arr, char *s1, char *s2, size_t len)
 	int n;
 	size_t len1;
 	size_t len2;
+	size_t sl1;
+	size_t sl2;
 
 	i = -1;
 	n = 0;
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	size_t sl1 = len1 - 1;
-	size_t sl2 = len2 - 1;
+	len1 = s1[strlen(s1) - 1] == '\n' ? strlen(s1) - 1 : strlen(s1);
+	len2 = s1[strlen(s2) - 1] == '\n' ? strlen(s2) - 1 : strlen(s2);
+	sl2 = len2 - 1;
 	while (++i < len1)
 	{
 		j = -1;
@@ -76,9 +90,10 @@ void mult_infinit(int *arr, char *s1, char *s2, size_t len)
 		n++;
 		sl2--;
 	}
+	print_answer(arr, len);
 }
 
-void read_file(char **str, char*path)
+void read_file(char **str, char *path)
 {
 	FILE *fp;
 	size_t len = 0;
@@ -102,17 +117,10 @@ int main(int ac, char **av)
 		read_file(&s1, av[1]);
 		read_file(&s2, av[2]);
 		len = strlen(s1) + strlen(s2);
-		arr = (int *)malloc(sizeof(int) * len);
+		arr = (int *) malloc(sizeof(int) * len);
 		bzero(arr, len * sizeof(int));
-//		memset(arr, 0, len * sizeof(int));
 		mult_infinit(arr, s1, s2, len);
-
 	}
 	else
 		ft_error(1);
-
-
-
-
-
 }
